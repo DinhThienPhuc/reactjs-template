0feb98af10c2bd5edbe364b626f133f0b9bb08ae
DinhThienPhuc <thienphuc.bka1412@gmail.com>
2022-09-11 23:03:41 +0700

    done for setting page

--- src/pages/setting/index.style.ts
+++ src/pages/setting/index.style.ts
@@ -11,6 +11,25 @@ const Styled = {
     background: ${({ theme }) => theme.colors.white};
     box-shadow: ${({ theme }) => theme.colors.boxShadows["100"]};
     border-radius: 10px;
+    padding: 60px 52px;
+  `,
+  Item: styled.div`
+    display: flex;
+    align-items: center;
+    color: ${({ theme }) => theme.colors.gray["100"]};
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 24px;
+    height: 30px;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.border["100"]};
+    margin-bottom: 50px;
+    padding: 0px 5px;
+  `,
+  Icon: styled.div`
+    width: 24px;
+    height: 24px;
+    margin-right: 24px;
+    color: ${({ theme }) => theme.colors.blue["200"]};
   `,
 };
 
--- src/pages/setting/index.tsx
+++ src/pages/setting/index.tsx
@@ -1,22 +1,65 @@
 /* PAGE: SETTING
    ========================================================================== */
 
+import { useCallback, useMemo } from "react";
+
 import { Header } from "components";
 import Icons from "assets/icons";
+import { ROUTES } from "utils/constants";
 import Styled from "./index.style";
+import { useNavigate } from "react-router-dom";
 import { useTranslation } from "services/i18n";
+
+const SETTING_ITEMS = [
+  {
+    icon: <Icons.Bell />,
+    label: "texts.notification",
+  },
+  {
+    icon: <Icons.FileText />,
+    label: "texts.privacy",
+  },
+  {
+    icon: <Icons.Shield />,
+    label: "texts.security",
+  },
+  {
+    icon: <Icons.HelpCircle />,
+    label: "texts.help",
+  },
+  {
+    icon: <Icons.Book />,
+    label: "texts.about",
+  },
+];
 
 const Setting = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const content = useMemo(() => {
+    return SETTING_ITEMS.map((item) => (
+      <Styled.Item key={item.label}>
+        <Styled.Icon>{item.icon}</Styled.Icon>
+        {t(item.label)}
+      </Styled.Item>
+    ));
+  }, [t]);
+
+  const goBack = useCallback(() => {
+    navigate(ROUTES.HOME);
+  }, [navigate]);
+
   return (
     <>
       <Header
         hasRightIcon={false}
         leftIcon={<Icons.ArrowLeft />}
         title={t("header.settings")}
+        onClickLeftIcon={goBack}
       />
       <Styled.Setting>
-        <Styled.Paper></Styled.Paper>
+        <Styled.Paper>{content}</Styled.Paper>
       </Styled.Setting>
     </>
   );
